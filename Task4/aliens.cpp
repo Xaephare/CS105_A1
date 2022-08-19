6f9e12e978fc61d93171d0bccefe2d42d817e785
Xaephare <louisgrace@onmail.com>
2022-08-19 16:09:44 +1200

    Complete

--- Task4/aliens.cpp
+++ Task4/aliens.cpp
@@ -1,79 +1,80 @@
 #include "aliens.h"
 #include <ctime>
 #include <cstdlib>
+#include <iostream>
 
 
-	Alien::Alien(int weight, int height, char gender) {
-		this->weight = weight;
-		this->height = height;
-		this->gender = gender;
+Alien::Alien(int weight, int height, char gender) {
+	this->weight = weight;
+	this->height = height;
+	this->gender = gender;
+}
+
+int Alien::get_weight() {
+	return weight;
+}
+
+int Alien::get_height() {
+	return height;
+}
+
+char Alien::get_gender() {
+	return gender;
+}
+
+int Alien::get_prestige() {
+	int gender_points;
+
+	if (gender == 'M') {
+		gender_points = 2;
 	}
-	
-	int Alien::get_weight() {
-		return weight;
-	}
-	
-	int Alien::get_height() {
-		return height;
-	}
-	
-	char Alien::get_gender() {
-		return gender;
-	}
-	
-	int Alien::get_prestige() {
-		int gender_points;
-		
-		if (gender == 'M') {
-			gender_points = 2;
-		}
-		else {
-			gender_points = 3;
-		}
-		
-		return (weight * height * gender_points);
+	else {
+		gender_points = 3;
 	}
 
-	//assignment
-	Alien Alien::operator+(Alien& other) {
-		srand(time(nullptr));
-		
-		int rand_binary = rand()%2;
-		
-		int new_weight = (weight + other.weight) / 2;
-		int new_height = (height + other.height) / 2;
-		
-		char new_gender;
-		if (rand_binary == 0) {
-			new_gender = 'M';
-		}
-		else {
-			new_gender = 'F';
-		}
-		
-		return Alien(new_weight, new_height, new_gender);
+	return (weight * height * gender_points);
+}
+
+//assignment
+Alien Alien::operator+(Alien& other) {
+
+	char new_gender;
+	int rand_binary = rand() % 2;
+	std::cout << rand_binary;
+
+	int new_weight = (weight + other.weight) / 2;
+	int new_height = (height + other.height) / 2;
+
+	if (rand_binary == 0) {
+		new_gender = 'M';
 	}
-	void Alien::operator=(Alien& other) {
-		weight = other.weight;
-		height = other.height;
-		gender = other.gender;
+	else {
+		new_gender = 'F';
 	}
-	//comparison
-	bool Alien::operator==(Alien& other) {
-		return get_prestige() == other.get_prestige();
-	}
-	bool Alien::operator!=(Alien& other) {
-		return get_prestige() != other.get_prestige();
-	}
-	bool Alien::operator>(Alien& other) {
-		return get_prestige() > other.get_prestige();
-	}
-	bool Alien::operator>=(Alien& other) {
-		return get_prestige() >= other.get_prestige();
-	}
-	bool Alien::operator<(Alien& other) {
-		return get_prestige() < other.get_prestige();
-	}
-	bool Alien::operator<=(Alien& other) {
-		return get_prestige() <= other.get_prestige();
-	}
+
+	return Alien(new_weight, new_height, new_gender);
+}
+void Alien::operator=(Alien& other) {
+	weight = other.weight;
+	height = other.height;
+	gender = other.gender;
+}
+//comparison
+bool Alien::operator==(Alien& other) {
+	return get_prestige() == other.get_prestige();
+}
+bool Alien::operator!=(Alien& other) {
+	return get_prestige() != other.get_prestige();
+}
+bool Alien::operator>(Alien& other) {
+	return get_prestige() > other.get_prestige();
+}
+bool Alien::operator>=(Alien& other) {
+	return get_prestige() >= other.get_prestige();
+}
+bool Alien::operator<(Alien& other) {
+	return get_prestige() < other.get_prestige();
+}
+bool Alien::operator<=(Alien& other) {
+	return get_prestige() <= other.get_prestige();
+}
---
+++ Task4/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "aliens.h"
+#include <vector>
+#include <cstdlib>
+
+std::vector <Alien> aliens;
+std::vector <Alien> offspring;
+
+void create_pairs() {
+	Alien alien1(50, 84, 'M');
+	Alien alien2(40, 70, 'F');
+	Alien alien3(55, 70, 'M');
+	Alien alien4(55, 40, 'F');
+
+	aliens.push_back(alien1);
+	aliens.push_back(alien2);
+	aliens.push_back(alien3);
+	aliens.push_back(alien4);
+	std::cout << "Alien pairs created.\n\n";
+}
+
+void create_offspring() {
+	if (aliens.size() < 4) {
+		std::cout << "Not enough aliens to create offspring\n\n";
+	}
+	else {
+		Alien alien5 = aliens[0] + aliens[1];
+		Alien alien6 = aliens[2] + aliens[3];
+		
+		offspring.push_back(alien5);
+		offspring.push_back(alien6);
+		std::cout << "Offspring created... (Alien5 & Alien6)\n\n";
+	}
+}
+
+void compare_offspring() {
+	if (offspring.size() < 2) {
+		std::cout << "Not enough offspring to compare.\n\n";
+	}
+	else {
+		std::cout << "Alien5 == Alien6 ? " << std::boolalpha << (offspring[0] == offspring[1]) << std::endl;
+		std::cout << "Alien5 != Alien6 ? " << std::boolalpha << (offspring[0] != offspring[1]) << std::endl;
+		std::cout << "Alien5 > Alien6 ? " << std::boolalpha << (offspring[0] > offspring[1]) << std::endl;
+		std::cout << "Alien5 >= Alien6 ? " << std::boolalpha << (offspring[0] >= offspring[1]) << std::endl;
+		std::cout << "Alien5 < Alien6 ? " << std::boolalpha << (offspring[0] < offspring[1]) << std::endl;
+		std::cout << "Alien5 <= Alien6 ? " << std::boolalpha << (offspring[0] <= offspring[1]) << std::endl;
+
+		std::cout << offspring[0].get_gender() << offspring[1].get_gender() << std::endl;
+	}
+}
+
+bool main_menu() {
+	int sel;
+	std::cout << "Enter your selection: ";
+	std::cin >> sel;
+
+	switch (sel) {
+	case 1:
+		create_pairs();
+		break;
+	case 2:
+		create_offspring();
+		break;
+	case 3:
+		compare_offspring();
+		break;
+	case 4:
+		return false;
+		break;
+	default:
+		std::cout << "Invalid selection\n";
+		break;
+	}
+	return true;
+}
+
+int main() {
+	srand(time(nullptr)); //must be called outside of function otherwise granularity is too low to give accurate results
+
+	
+	std::cout << "Main menu:\n";
+	std::cout << "	1. Create alien pairs.\n";
+	std::cout << "	2. Create offspring.\n";
+	std::cout << "	3. Compare offspring prestige.\n";
+	std::cout << "	4. Exit\n";
+	while (main_menu()) {};
+
+	return 0;
+}
